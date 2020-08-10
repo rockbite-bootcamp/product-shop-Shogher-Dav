8ed30f41407307477d53574c4bc3fdf6c144dcb1
Shogher Davtyan <shogher.davtyan@mauriceward.com>
2020-08-10 15:23:29 +0400

    Create a shop with Command Pattern

---
+++ src/com/rockbite/bootcamp/Main.java
@@ -0,0 +1,18 @@
+package com.rockbite.bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Vochxar");
+
+
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     */
+    public void  newMethid (String x,  String y ) {
+
+    }
+}
